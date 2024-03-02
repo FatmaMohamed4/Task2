ef130572b2fd84aa6d9bbe04d61a9f4a4f1ab6cd
FatmaMohamed4 <157400371+FatmaMohamed4@users.noreply.github.com>
2024-03-02 15:47:48 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const request = require("request")
-const geocode= require("./data/geoCode.js")
+// const geocode= require("./data/geoCode.js")
 const forecast = require('./data/forecast.js')
 
 const url = "https://api.weatherapi.com/v1/current.json?key=7f97e74ef23b418c97a155211230503&q=egypt"
@@ -26,10 +26,10 @@ request ({url, json : true  } , (error , response) => {
     }
 })
 
-
-const geocodeUrl ='https://api.mapbox.com/geocoding/v5/mapbox.places/egypt.json?access_token=pk.eyJ1IjoiaXNsYW0yODQiLCJhIjoiY2wwamEzNmFhMGFtNTNkb3pqaXk4bXNnYSJ9.qYlrWIqo41gXgNNc4h8yIw'
+ 
+const geocodeUrl1 ='https://api.mapbox.com/geocoding/v5/mapbox.places/egypt.json?access_token=pk.eyJ1IjoiaXNsYW0yODQiLCJhIjoiY2wwamEzNmFhMGFtNTNkb3pqaXk4bXNnYSJ9.qYlrWIqo41gXgNNc4h8yIw'
 //run
-request ({url : geocodeUrl , json : true} , (error , response) => {
+request ({url : geocodeUrl1 , json : true} , (error , response) => {
      
         if (error){
             callback ("unable to connect geocode service" , undefined)
@@ -39,3 +39,37 @@ request ({url : geocodeUrl , json : true} , (error , response) => {
              
         }
       })
+    
+
+
+
+const geocode = ( address , callback) => {
+const geocodeUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + address +  ".json?access_token=pk.eyJ1IjoiaXNsYW0yODQiLCJhIjoiY2wwamEzNmFhMGFtNTNkb3pqaXk4bXNnYSJ9.qYlrWIqo41gXgNNc4h8yIw"
+request ({url : geocodeUrl , json : true} , (error , response) => {
+     
+            if (error){
+                callback ("unable to connect geocode service" , undefined)
+            }else if (response.body.message)  {
+                callback (response.body.message , undefined )
+            } else if (response.body.features.length == 0) {
+                 callback("Unable to find location" , undefined)
+            } else {
+                callback(undefined , {
+                     longtitude : response.body.features[0].center[0],
+                     latitude : response.body.features[0].center[1]
+                } )
+               
+            }
+ })       
+}
+
+const country = process.argv[2]
+geocode( country , (error , data) => {
+    console.log("ERROR : " , error)
+    console.log("DATA : "  , data)
+
+    forecast( data.latitude , data.longtitude , (error , data) => {
+        console.log("ERROR : " , error)
+        console.log("DATA : " , data)
+     } )
+ })
